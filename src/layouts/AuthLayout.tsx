5eb683a6a92f365fc061eec2a23adbf91ceacb89
javiers78889 <javiers78889@gmail.com>
2024-12-17 15:37:58 -0500

    primer commit

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+
+export default function AuthLayout() {
+    return (
+        <div className=" bg-slate-800 min-h-screen">
+            <div className="max-w-lg mx-auto pt-10 px-5">
+                <img src="/logo.svg" alt="" />
+            </div>
+            <div className="py-10 max-w-lg mx-auto pt-10 px-5">
+                <Outlet />
+            </div>
+        </div>
+    )
+}
---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginViews from './views/LoginViews'
+import RegisterViews from './views/RegisterViews'
+import AuthLayout from './layouts/AuthLayout'
+
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AuthLayout />}>
+                    <Route path='/auth/login' element={<LoginViews />} />
+                    <Route path='/auth/register' element={<RegisterViews />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
