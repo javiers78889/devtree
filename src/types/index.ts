84d4841dc6c436bbbefb8eb2565afb0036aa09e3
javiers78889 <javiers78889@gmail.com>
2024-12-18 23:10:39 -0500

    mejora login

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,4 +8,6 @@ export type RegisterForm = Pick<User, 'handle' | 'email' | 'name'> & {
 
     password: string
     password_confirmation: string
-}  
+}
+
+export type LoginForm = Pick<User, 'email' | 'password'>
--- src/views/LoginViews.tsx
+++ src/views/LoginViews.tsx
@@ -1,14 +1,27 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { LoginForm } from "../types";
+import ErrorMessage from "../components/ErrorMessage";
 
 export default function LoginViews() {
+    const initialValues: LoginForm = {
+        email: '',
+        password: '',
+
+    }
+    const { register, reset, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+    const handlerLogin = (datos: LoginForm) => {
+        console.log(datos)
+        reset()
+    }
     return (
         <>
             <h1 className="text-4xl text-white font-bold">Login</h1>
             <form
-                onSubmit={() => { }}
+                onSubmit={handleSubmit(handlerLogin)}
                 className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
             >
-            
+
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
                     <input
@@ -16,9 +29,14 @@ export default function LoginViews() {
                         type="email"
                         placeholder="Email de Registro"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email', { required: "El email es obligatorio" , pattern: {
+                            value: /\S+@\S+\.\S+/,
+                            message: "E-mail no válido",
+                        },})}
                     />
+                    {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
                 </div>
-               
+
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
                     <input
@@ -26,7 +44,11 @@ export default function LoginViews() {
                         type="password"
                         placeholder="Password de Registro"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', {
+                            required: 'El password es requerido'
+                        })}
                     />
+                    {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
                 </div>
 
                 <input
